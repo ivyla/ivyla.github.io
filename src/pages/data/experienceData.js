08cb5b449522b6f4afc05a089cab97c0a84cd865
Ivy <ivyla3@gmail.com>
2022-01-10 20:53:56 -0800

    added links for experience, tweaks to projects and experience tiles

--- src/pages/data/experienceData.js
+++ src/pages/data/experienceData.js
@@ -1,57 +1,68 @@
 const experienceData = [
     {
         key: 1,
-        role: "Software Engineer",
+        role: "Software Developer",
         company: "Charles Schwab",
         companyLink: "https://www.schwab.com/",
-        roleDesc:
-            " - Backend developer for investment performance -Enhancing professional and technical skills in the NERD program",
+        roleDesc: [
+            "Java developer for investment performance",
+            "Enhancing professional and technical skills in the NERD program"
+        ],
         date: "July 2021 - Present"
         // image: "../images/schwab.png"
     },
     {
-        key: 2,
-        role: "Developer, Scrum Master",
-        company: "Rent-A-Driveway",
-        companyLink:
-            "https://docs.google.com/presentation/d/1TTCm3boRhFMU7ZIW47mZuz1chMMkxtGQ4POKX7DZcOE/edit?usp=sharing",
-        roleDesc:
-            "- Fullstack development using Flutter and Firebase - Agile sprint planning and leading weekly scrum meetings -Design wireframes for mobile and web UI",
-        date: "Sept. 2019 - June 2020"
-        // image: "../images/readlee.png"
-    },
-    {
-        key: 3,
-        role: "UX Research Assistant",
-        company: "UCSC Tech4Good Lab",
-        companyLink: "https://tech4good.soe.ucsc.edu/#/",
-        roleDesc:
-            "-Contributed to a research paper by utilizing qualitative coding and discussion groups -Facilitate discussion and share insights to develop user personas",
-        date: "Jan. 2019 - June 2019"
-        // image: "../images/smile-small.png"
-        // https://tech4good.soe.ucsc.edu/assets/docs/sigcse-2021-ex.pdf
-    },
-    {
-        key: 4,
-        role: "Family Lead",
-        company: "UCSC Chinese Student Association",
-        companyLink: "https://www.facebook.com/CSAUCSC",
-        roleDesc:
-            "- Plan and advertise events at weekly meetings - Foster a friendly and welcoming environment for new and current members",
-        date: "Nov. 2020 - Jan. 2021"
-        // image: "../images/csa.jpg"
-    },
-    {
         key: 5,
-        role: "Software Engineer",
+        role: "Software Developer",
         company: "Readlee",
         companyLink: "https://readlee.com",
-        roleDesc:
-            "- Frontend development using Flutter framework -Develop user interfaces for reading comprehension software",
+        roleDesc: [
+            "Frontend development using Flutter framework",
+            "Develop user interfaces for reading comprehension software"
+        ],
         // Provide enhancements to user
         date: "Nov. 2020 - Jan. 2021"
         // image: "../images/csa.jpg"
+    },
+    {
+        key: 2,
+        role: "Founding Member",
+        company: "Rent-A-Driveway",
+        companyLink:
+            "https://docs.google.com/presentation/d/1TTCm3boRhFMU7ZIW47mZuz1chMMkxtGQ4POKX7DZcOE/edit?usp=sharing",
+        roleDesc: [
+            "Fullstack development using Flutter and Firebase",
+            "Agile sprint planning and leading weekly scrum meetings",
+            "Design wireframes for mobile and web UI"
+        ],
+        date: "Sept. 2019 - June 2020"
+        // image: "../images/readlee.png"
     }
+    // {
+    //     key: 3,
+    //     role: "UX Research Assistant",
+    //     company: "UCSC Tech4Good Lab",
+    //     companyLink: "https://tech4good.soe.ucsc.edu/#/",
+    //     roleDesc: [
+    //         "Contributed to a research paper by utilizing qualitative coding and discussion groups",
+    //         "Facilitate discussion and share insights to develop user personas"
+    //     ],
+    //     date: "Jan. 2019 - June 2019"
+    //     // image: "../images/smile-small.png"
+    //     // https://tech4good.soe.ucsc.edu/assets/docs/sigcse-2021-ex.pdf
+    // },
+    // {
+    //     key: 4,
+    //     role: "Family Lead",
+    //     company: "UCSC Chinese Student Association",
+    //     companyLink: "https://www.facebook.com/CSAUCSC",
+    //     roleDesc: [
+    //         "Plan and advertise events at weekly meetings",
+    //         "Foster a friendly and welcoming environment for new and current members"
+    //     ],
+    //     date: "Nov. 2020 - Jan. 2021"
+    //     // image: "../images/csa.jpg"
+    // },
 ]
 
 export default experienceData
