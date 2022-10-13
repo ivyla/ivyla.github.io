e18dccc457db4436ebf90d352c3f16f422d131a5
Ivy <ivyla3@gmail.com>
2022-10-12 21:23:13 -0700

    Cant transfer skills data props for some reason

---
+++ src/pages/data/skillsData.js
@@ -0,0 +1,57 @@
+// const skillsData = [
+//     {
+//         key: 1,
+//         skillsTitle: "Languages",
+//         skillsList: ["Java", "Javascript", "Python", "SQL", "HTML", "CSS"],
+//         columnNums: 2
+//     },
+//     {
+//         key: 2,
+//         skillsTitle: "Frameworks/Tools",
+//         skillsList: [
+//             "Java Spring",
+//             "Spring Data JPA",
+//             "Pivotal Cloud Foundry (PCF)",
+//             "Junit",
+//             "Atlassian Bamboo",
+//             "React.js",
+//             "Redux",
+//             "Axios",
+//             "Pytest",
+//             "Docker"
+//         ],
+//         columnNums: 2
+//     }
+//     // {
+//     //     key: 3,
+//     //     skillsTitle: "Development",
+//     //     skillsList: [
+//     //         "REST APIs",
+//     //         "Batch Processing",
+//     //         "Object-Oriented Programming",
+//     //         "Computer Systems Design",
+//     //         "Zephyr Testing",
+//     //         "Behavior Driven Development (BDD)",
+//     //         "Test Driven Development (TDD)",
+//     //         "Front End Development + Design"
+//     //     ],
+//     //     columnNums: 2
+//     // },
+//     // {
+//     //     key: 4,
+//     //     skillsTitle: "Frameworks/Tools",
+//     //     skillsList: [
+//     //         "Agile Practices",
+//     //         "Public Speaking",
+//     //         "High Fidelity Mockups",
+//     //         "Wireframing",
+//     //         "User Persona Research",
+//     //         "Qualitative Coding",
+//     //         "Ideation",
+//     //         "Video Editing"
+//     //     ],
+//     //     columnNums: 2
+//     // }
+// ]
+
+// export default skillsData
